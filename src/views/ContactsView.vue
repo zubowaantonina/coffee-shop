ab33bc6d8070802b381ba9366f14cae67acbf213
zubowaantonina <zubowaantonina@yandex.ru>
2024-06-16 11:48:25 +0300

    Vuelidate

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -23,40 +23,64 @@
 
             <form @submit.prevent="submit" action="#" class="mt-5">
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="name-input" class="mb-0">
                     Name
                     <span style="color: red">*</span>
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input  v-model="name" type="text" class="form-control" id="name-input" />
+                  <input
+                    v-model="v$.name.$model"
+                    type="text"
+                    class="form-control"
+                    id="name-input"
+                  />
+                  <span v-for="error in v$.name.$errors"
+                  :key="error.$uid"
+                  >{{ error.$message }}</span>
                 </div>
               </div>
-<pre>{{ v$ }}</pre>
+             
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="email-input" class="mb-0">
                     E-mail
                     <span style="color: red">*</span>
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input  v-model="email" type="email" class="form-control" id="email-input" />
+                  <input
+                    v-model="v$.email.$model"
+                    type="email"
+                    class="form-control"
+                    id="email-input"
+                  />
+                  <span v-for="error in v$.email.$errors"
+                  :key="error.$uid"
+                  >{{ error.$message }}</span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="phone-input" class="mb-0"> Phone </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input  v-model="phone" type="tel" class="form-control" id="phone-input" />
+                  <input
+                    v-model="v$.phone.$model"
+                    type="tel"
+                    class="form-control"
+                    id="phone-input"
+                  />
+                  <span v-for="error in v$.phone.$errors"
+                  :key="error.$uid"
+                  >{{ error.$message }}</span>
                 </div>
               </div>
 
               <div class="form-group row textarea">
-                <div class="col col-12 d-flex justify-content-center">
+                <div class="col col-12 d-flex justify-content-start">
                   <label for="pmessage" class="mb-3 mt-3 text-center">
                     Your message
                     <span style="color: red">*</span>
@@ -64,14 +88,18 @@
                 </div>
                 <div class="col col-12">
                   <textarea
-                    v-model="message"
+                    v-model="v$.message.$model"
                     class="form-control"
                     name="message"
                     id="message"
                     rows="5"
                     placeholder="Leave your comments here"
                   ></textarea>
+                  <span v-for="error in v$.message.$errors"
+                  :key="error.$uid"
+                  >{{ error.$message }}</span>
                 </div>
+               
               </div>
 
               <div class="row">
@@ -88,34 +116,47 @@
 </template>
 <script>
 import NavBarComponent from "@/components/NavBarComponent.vue";
-import { useVuelidate } from '@vuelidate/core'
-import { required, email } from '@vuelidate/validators'
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, maxLength } from "@vuelidate/validators";
+import { helpers } from '@vuelidate/validators'
+import { minLength } from '../validators/minLengh';
+
 
 export default {
-  setup () {
-    return { v$: useVuelidate() }
+  setup() {
+    return { v$: useVuelidate() };
   },
   data() {
     return {
       name: "",
-        email: "",
-        phone: "",
-        message: ""
+      email: "",
+      phone: "",
+      message: "",
     };
   },
   components: { NavBarComponent },
-  validations () {
+  validations() {
     return {
-      name:  { required },
-        email:  { required, email },
-        phone:  { required },
-        message:  {  }
-    }
+      name: { required },
+      email: { required, email },
+      phone: {  },
+      message: {required,
+         maxLength:maxLength(20),
+          minLength: helpers.withMessage('this value min 5', minLength)
+        },
+    };
   },
-  methods:{
-    submit(){
-        // console.log(this.form);
-    }
-  }
+  methods: {
+   async submit() {
+      const isFormCorrect = await this.v$.$validate()
+      if (!isFormCorrect) return
+     console.log(
+    {  name: this.name,
+      email: this.email,
+      phone: this.phone,
+      message: this.message,}
+     );
+    },
+  },
 };
 </script>
